a1c6982e2d9072254ccc29009539628118629f48
gabriel <gabriel.romano.3194@hotmail.com>
2022-03-24 15:40:10 -0300

    alterando configuração do projeto

--- target/classes/data.sql
+++ target/classes/data.sql
@@ -1,7 +1,7 @@
 INSERT INTO `users` (`user_id`, `email`, `enabled`, `username`, `role`, `password`) VALUES
-(1,	'fulano@gmail.com',	CONV('1', 2, 10) + 0,	'fulano',	'USER',	'$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i'),
-(2,	'cigano@gmail.com',	CONV('1', 2, 10) + 0,	'cigano',	'USER',	'$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i'),
-(3,	'beltrano@gmail.com',	CONV('1', 2, 10) + 0,	'beltrano',	'USER',	'$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i');
+(1,	'fulano@gmail.com',	true,	'fulano',	'USER',	'$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i'),
+(2,	'cigano@gmail.com',	true,	'cigano',	'USER',	'$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i'),
+(3,	'beltrano@gmail.com',	true,	'beltrano',	'USER',	'$2a$10$8MeF8YTUTv22DVthkhOs3.WGT4W1Wp1xRXcRxTM12MgDzRviDpw7i');
 
 INSERT INTO `leilao` (`id`, `data_abertura`, `nome`, `valor_inicial`, `usuario_user_id`) VALUES
 (1,	'2020-08-03',	'Tablet Xpto 3',	5.00,	1),
