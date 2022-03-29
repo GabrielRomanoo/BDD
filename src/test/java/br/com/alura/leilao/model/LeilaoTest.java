5cfce86f77311dca53ea3bda622bed4facaf07e6
gabriel <gabriel.romano.3194@hotmail.com>
2022-03-29 14:08:23 -0300

    usando display name

--- src/test/java/br/com/alura/leilao/model/LeilaoTest.java
+++ src/test/java/br/com/alura/leilao/model/LeilaoTest.java
@@ -8,16 +8,27 @@ import org.junit.jupiter.api.Test;
 
 public class LeilaoTest {
 	
-	@Test
-    public void deveReceberUmLance() {
+    @Nested
+    @DisplayName("Dado um lance valido")
+    class UnicoLanceTests {
+
         Leilao leilao = new Leilao("Macbook Pro 15");
-        assertEquals(0, leilao.getLances().size());
-
         BigDecimal doisMil = new BigDecimal("2000.0");
-		leilao.propoe(new Lance(new Usuario("Steve Jobs"), doisMil));
 
-        assertEquals(1, leilao.getLances().size());
-        assertEquals(doisMil, leilao.getLances().get(0).getValor());
+        @Nested
+        @DisplayName("Quando propoe o lance")
+        class AssertLanceNormal {
+
+            @Test
+            @DisplayName("Entao o lance eh aceito")
+            public void deveReceberUmLance() {
+
+                leilao.propoe(new Lance(new Usuario("Steve Jobs"), doisMil));
+
+                assertEquals(1, leilao.getLances().size());
+                assertEquals(doisMil, leilao.getLances().get(0).getValor());
+            }
+        }
     }
 	
 	@Test
